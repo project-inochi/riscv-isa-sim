0f9289ffc09125ae1271fe7f3d2f19ebc6795884
Inochi Amaoto <inochiama@gmail.com>
2026-06-03 18:11:50 +0800

    spike: match Vexii medeleg WARL mask

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -1139,22 +1139,14 @@ void medeleg_csr_t::verify_permissions(insn_t insn, bool write) const {
 
 bool medeleg_csr_t::unlogged_write(const reg_t val) noexcept {
   const reg_t mask = 0
-    | (proc->extension_enabled(EXT_ZCA) ? 0 : 1 << CAUSE_MISALIGNED_FETCH)
-    | (1 << CAUSE_FETCH_ACCESS)
-    | (1 << CAUSE_ILLEGAL_INSTRUCTION)
+    | (1 << CAUSE_MISALIGNED_FETCH)
     | (1 << CAUSE_BREAKPOINT)
-    | (1 << CAUSE_MISALIGNED_LOAD)
-    | (1 << CAUSE_LOAD_ACCESS)
-    | (1 << CAUSE_MISALIGNED_STORE) 
-    | (1 << CAUSE_STORE_ACCESS)
     | (1 << CAUSE_USER_ECALL)
     | (1 << CAUSE_SUPERVISOR_ECALL)
     | (proc->has_mmu() ? mmu_exceptions : 0)
     | (proc->extension_enabled('H') ? hypervisor_exceptions : 0)
-    | (1 << CAUSE_SOFTWARE_CHECK_FAULT)
-    | (1 << CAUSE_HARDWARE_ERROR_FAULT)
     ;
-  return basic_csr_t::unlogged_write(val & mask);
+  return basic_csr_t::unlogged_write((read() & ~mask) | (val & mask));
 }
 
 sip_csr_t::sip_csr_t(processor_t* const proc, const reg_t addr, generic_int_accessor_t_p accr):
